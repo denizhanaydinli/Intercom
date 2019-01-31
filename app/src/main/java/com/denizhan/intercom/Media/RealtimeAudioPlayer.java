75e42e520e5225bf9d50a808caf7f2445ef6eb52
mabasturk <m.a.basturk@gmail.com>
2019-01-31 20:58:24 +0300

    Realtime audio player updated

--- app/src/main/java/com/denizhan/intercom/Media/RealtimeAudioPlayer.java
+++ app/src/main/java/com/denizhan/intercom/Media/RealtimeAudioPlayer.java
@@ -5,11 +5,19 @@ package com.denizhan.intercom.Media;
 */
 
 import com.denizhan.intercom.Interfaces.ActivityMediaInteractionInterface;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 
 public class RealtimeAudioPlayer implements ActivityMediaInteractionInterface {
 
-    public RealtimeAudioPlayer(){
+    private AudioTrack audioTrack; // Gerçek zamanlı ses oynatı class
 
+    public RealtimeAudioPlayer(){
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 8000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, AudioTrack.getMinBufferSize(1000, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT), AudioTrack.MODE_STREAM );
+        // 8000 hertz de 16 bit çözünürlüğünde tek kanalda kayıt yap, 1000'er 1000'er bufferla sesi oynat
+        audioTrack.play();
+        // yazmadan önce çağırılması gerekiyor
     }
 
     @Override
@@ -24,11 +32,18 @@ public class RealtimeAudioPlayer implements ActivityMediaInteractionInterface {
 
     @Override
     public void stop() {
-
+        if(audioTrack != null){
+            audioTrack.stop();
+        }
     }
 
     @Override
     public void destroy() {
-
+        if(audioTrack != null){
+            stop();
+            audioTrack.release();
+            audioTrack = null;
+        }
     }
+
 }
