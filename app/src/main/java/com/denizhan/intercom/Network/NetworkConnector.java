6a866ad3055392eda76fa5eb73d5e87da29f7255
NehirDemir <demirnehir2@gmail.com>
2019-02-14 17:05:58 +0300

    ByteSplitter class duzenlendi.

--- app/src/main/java/com/denizhan/intercom/Network/NetworkConnector.java
+++ app/src/main/java/com/denizhan/intercom/Network/NetworkConnector.java
@@ -91,6 +91,31 @@ public class NetworkConnector {
 
         //bytelari parcalara ayirma islemi yapacak
         //  Arrays.copyOfRange();
+        private byte[] data;
+        private int PIECE_SIZE;
+        private int REMAINING;
+        private int INDEX;
+
+        public ByteSplitter(byte[] data, int pieceSize)
+        {
+            this.data = data;
+            this.PIECE_SIZE = pieceSize;
+            this.REMAINING = data.length;
+            this.INDEX = 0;
+        }
+
+        public byte[] nextData(){
+            if(this.REMAINING > 0){
+                if(this.REMAINING > this.PIECE_SIZE){
+                    byte[] data_piece = Arrays.copyOfRange(data, this.INDEX, this.INDEX + this.PIECE_SIZE);
+                    this.INDEX += this.PIECE_SIZE;
+                    this.REMAINING -= this.PIECE_SIZE;
+                    return data_piece; // parça boyutu kadar olan datayı ver
+                }
+            }
+            return null;
+        }
+
     }
     public enum COMMANDS
     {
