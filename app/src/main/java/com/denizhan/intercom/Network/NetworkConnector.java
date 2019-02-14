f5330a53fed16a4be85bcf6002426aac082b3b55
NehirDemir <demirnehir2@gmail.com>
2019-02-14 18:15:44 +0300

    DateQueue classi eklendi.

--- app/src/main/java/com/denizhan/intercom/Network/NetworkConnector.java
+++ app/src/main/java/com/denizhan/intercom/Network/NetworkConnector.java
@@ -12,6 +12,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Arrays;
 import java.util.Objects;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
+
+import java.util.Queue;
 
 public class NetworkConnector {
 
@@ -99,6 +104,38 @@ public class NetworkConnector {
             e.printStackTrace();
         }
         return file_data;
+    }
+    public class DateQueue
+
+    {
+
+        private SimpleDateFormat dateFormat;
+
+        private Queue<String> ringDateQueue = new LinkedList<>();
+        private Queue<String> textDateQueue = new LinkedList<>();
+        private Queue<String> audioDateQueue = new LinkedList<>();
+        private Queue<String> videoDateQueue = new LinkedList<>();
+
+        public DateQueue(){
+            dateFormat = new SimpleDateFormat("EEE d MMM yyyy HH:mm:ss");
+        }
+
+        public void addRingDate(){
+            ringDateQueue.add(dateFormat.format(Calendar.getInstance().getTime()));
+        }
+
+        public void addTextDate(){
+            textDateQueue.add(dateFormat.format(Calendar.getInstance().getTime()));
+        }
+
+        public void addAudioDate(){
+            audioDateQueue.add(dateFormat.format(Calendar.getInstance().getTime()));
+        }
+
+        public void addVideoDate(){
+            videoDateQueue.add(dateFormat.format(Calendar.getInstance().getTime()));
+        }
+
     }
     public class ByteSplitter{
 
